e27270eebe839975fc52ee0ffe8972a87519df95
arektlen <arkadiusz.bejger@gmail.com>
2016-01-15 10:07:37 +0100

    form1

--- Lotto/Lotto/Form1.Designer.cs
+++ Lotto/Lotto/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.losuj1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // losuj1
+            // 
+            this.losuj1.Location = new System.Drawing.Point(101, 166);
+            this.losuj1.Name = "losuj1";
+            this.losuj1.Size = new System.Drawing.Size(83, 64);
+            this.losuj1.TabIndex = 0;
+            this.losuj1.Text = "Losuj";
+            this.losuj1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(75, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.losuj1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button losuj1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
