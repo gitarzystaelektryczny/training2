d1b775fe6b33609fd86ed4e226e5d7a27a61c34f
arektlen <arkadiusz.bejger@gmail.com>
2016-01-16 12:00:21 +0100

    GitHUB

--- Lotto/Lotto/Lotto/Logic.cs
+++ Lotto/Lotto/Lotto/Logic.cs
@@ -21,7 +21,7 @@ namespace Lotto
             for (int i = 0; i < 6; i++)
             {
                 //wydaje mi się, że zakres musi być o jedną liczbę większą ;)
-                a = rand.Next(1, 47); 
+                a = rand.Next(1, 7); 
 
                 //też nie wiedziałem, że jest taka metoda Contains();
                 if (!tab.Contains(a))
